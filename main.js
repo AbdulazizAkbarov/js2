23fd9e589cc67eefa57c72aafc2278072aa34c33
Abdulaziz <abdulazizakbarov065@gmail.com>
2024-10-15 21:52:41 +0500

    Finished

---
+++ main.js
@@ -0,0 +1,171 @@
+
+//1-Misol 
+
+// let uzunlik =prompt("Uzunlikni kirgizing");
+
+// let a = uzunlik/100;
+
+// alert (parseInt(a));
+
+
+
+ //1-Tugashi
+
+
+
+//2-Misol//
+
+// let ogirlik =   prompt("Og'irikni Kirgizing");
+
+// let a = ogirlik/1000;
+
+// alert (parseInt(a));
+
+
+//2-Tugashi
+
+
+//3-Misol//
+
+// let faylHajmi = prompt("Fayl Hajmini Baytda Kirgizing");
+
+// let a = faylHajmi/1024;
+
+// alert (parseInt(a));
+
+
+//3-Tugashi
+
+
+//4-Misol//
+
+// let a =prompt("A-sonini Kirgizing");
+
+// let b = prompt("B-sonini Kirgizing");
+
+// let c= a/b;
+
+// alert (parseInt(c));
+
+
+
+//4-Tugashi//
+
+
+//5-Misol//
+
+// let a =prompt("A-sonini Kirgizing");
+
+// let b = prompt("B-sonini Kirgizing");
+
+// let c= a%b;
+
+// alert (parseInt(c));
+
+
+
+//5-Tugashi//
+
+
+//6-Misol//
+
+// let a = prompt("2-Xonalik Son Kirgizing");
+
+// let b=a/10;
+// let c =a%10;
+
+
+// alert (`Onliklar Xonasidagi son ${parseInt(b)}, Birliklr Xonasidagi son: ${parseInt(c)}`);
+
+
+//6-Tugashi//
+
+//7-Misol//
+
+// let a = prompt("2-Xonalik Son Kirgizing");
+
+// let b=a/10;
+// let c =a%10;
+
+// let d = b+c;
+
+// alert( parseInt(d) );
+
+
+//7-Tugashi//
+
+
+//8-Misol//
+
+// let a = prompt("2-Xonalik Son Kirgizing");
+
+// let b=a/10;
+// let c =a%10;
+// alert(parseInt(`${c}${b}`));
+
+
+//8-Tugashi//
+
+
+//9-Misol//
+
+// let a = prompt("3-Xonalik Son Kirgizing");
+
+// let b=a/100;
+
+// alert(parseInt(b));
+
+
+//9-Tugashi//
+
+
+//10-Misol//
+
+// let a = prompt("3-Xonalik Son Kirgizing");
+// let c=a%10;
+// let d=a/10%10;
+// let b=a/100;
+// alert(parseInt(`${c}${d}${b}`));
+
+
+//10-Tugashi//
+
+ 
+ //11-Misol//
+
+    // let a = +prompt(" 3-xonali son kiriting")
+    // a =  parseFloat(a)
+    // let z = parseInt (a/100)
+    // let x = parseInt (a/10%10)
+    // let c = parseFloat (a%10)
+    // let natija = parseFloat(z+x+c)
+    // alert(`
+    //     Uch xonali sonni xar bir raqamlarining yig'indisi 
+    //     ${natija}`)
+
+//11-Tugashi//\
+
+
+
+//12-Misol//
+// let a = prompt ("3- xonali son kiriting")
+// a =  parseFloat(a)
+// let q = parseInt(a/100)
+// let w = parseInt (a/10%10)
+// let e = parseInt(a%10)
+// let natija = parseInt(e,w,q)
+// alert(`Natija ${e}${w}${q}
+//     `)
+
+// 12-Tugashi
+
+
+//   13-Misol
+
+// let b = +prompt("13-savol uch xonali son kiriting")
+// b = parseFloat (b)
+
+// let s = parseInt(b/100)
+// let a = parseInt(b%100*10+s)
+// alert(`
+//   Natija  ${a}`)
